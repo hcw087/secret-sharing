bafd82de2ac8f625a9db354b61eda443543b63d3
hcw087 <117704160+hcw087@users.noreply.github.com>
2023-07-28 18:34:18 +0800

    Update primes.py

--- secretsharing/primes.py
+++ secretsharing/primes.py
@@ -9,15 +9,7 @@
 
 
 def calculate_mersenne_primes():
-    """ Returns all the mersenne primes with less than 500 digits.
-        All primes:
-        3, 7, 31, 127, 8191, 131071, 524287, 2147483647L, 2305843009213693951L,
-        618970019642690137449562111L, 162259276829213363391578010288127L,
-        170141183460469231731687303715884105727L,
-        68647976601306097149...12574028291115057151L, (157 digits)
-        53113799281676709868...70835393219031728127L, (183 digits)
-        10407932194664399081...20710555703168729087L, (386 digits)
-    """
+    """ Returns all the Mersenne primes with less than 500 digits. """
     mersenne_prime_exponents = [
         2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607, 1279
     ]
@@ -29,6 +21,7 @@ def calculate_mersenne_primes():
         prime -= 1
         primes.append(prime)
     return primes
+
 
 SMALLEST_257BIT_PRIME = (2**256 + 297)
 SMALLEST_321BIT_PRIME = (2**320 + 27)
