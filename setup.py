1eb9011824c94210306407254a16471e0731d60d
hcw087 <117704160+hcw087@users.noreply.github.com>
2023-07-28 18:53:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.10',
     ],
 )
