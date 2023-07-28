480c00462c4fa8029319cc5a6d52bf9ddd6c5c1f
hcw087 <117704160+hcw087@users.noreply.github.com>
2023-07-28 18:51:52 +0800

    Update primes.py

--- secretsharing/primes.py
+++ secretsharing/primes.py
@@ -1,15 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-    Secret Sharing
-    ~~~~~
-
-    :copyright: (c) 2014 by Halfmoon Labs
-    :license: MIT, see LICENSE for more details.
-"""
-
-
 def calculate_mersenne_primes():
-    """ Returns all the Mersenne primes with less than 500 digits. """
     mersenne_prime_exponents = [
         2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607, 1279
     ]
@@ -33,11 +22,7 @@ STANDARD_PRIMES.sort()
 
 
 def get_large_enough_prime(batch):
-    """ Returns a prime number that is greater all the numbers in the batch.
-    """
-    # build a list of primes
     primes = STANDARD_PRIMES
-    # find a prime that is greater than all the numbers in the batch
     for prime in primes:
         numbers_greater_than_prime = [i for i in batch if i > prime]
         if len(numbers_greater_than_prime) == 0:
